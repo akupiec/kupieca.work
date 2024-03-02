fe932d1b916585af5a337d475b703b4d53398d5b
akupiec <kupiecart@gmail.com>
2024-03-02 19:12:59 +0100

    fix: footer

--- src/home/footer/footer.tsx
+++ src/home/footer/footer.tsx
@@ -3,16 +3,17 @@ export const Footer = () => (
     <h1>Interested in collaboration?</h1>
     <div class="person">
       <h5>Senior Software Developer</h5>
-      <a class="mail" href="mailto:art@akupiec.work">
+      <a class="mail" href="mailto:art@akupiec.work" target="_blank">
         art@akupiec.work
       </a>
       <a
         class="social linkedin"
         href="https://www.linkedin.com/in/artur-k-5b381b17b/"
-        aria-label="Open linkedin profile">
+        aria-label="Open linkedin profile"
+        target="_blank">
         <img src="./assets/social/linkedin.svg" alt="linkedin" role="presentation" />
       </a>
-      <a class="social github" href="https://github.com/akupiec" aria-label="Open github profile">
+      <a class="social github" href="https://github.com/akupiec" aria-label="Open github profile" target="_blank">
         <img src="./assets/social/github.svg" alt="github" role="presentation" />
       </a>
     </div>
@@ -21,20 +22,29 @@ export const Footer = () => (
       <a class="mail" href="mailto:ala@akupiec.work">
         ala@akupiec.work
       </a>
-      <a class="social linkedin" aria-label="Open linkedin profile" href="https://www.linkedin.com/in/alicja-kupiec/">
+      <a
+        class="social linkedin"
+        aria-label="Open linkedin profile"
+        href="https://www.linkedin.com/in/alicja-kupiec/"
+        target="_blank">
         <img src="./assets/social/linkedin.svg" alt="linkedin" role="presentation" />
       </a>
-      <a class="social figma" aria-label="Open figma profile" href="https://www.figma.com/@alicjakupiec">
+      <a
+        class="social figma"
+        aria-label="Open figma profile"
+        href="https://www.figma.com/@alicjakupiec"
+        target="_blank">
         <img src="./assets/social/figma.svg" alt="figma" role="presentation" />
       </a>
-      <a class="social behance" aria-label="Open behance profile" href="https://www.behance.net/alicjakupiec">
+      <a
+        class="social behance"
+        aria-label="Open behance profile"
+        href="https://www.behance.net/alicjakupiec"
+        target="_blank">
         <img src="./assets/social/behance.svg" alt="behance" role="presentation" />
       </a>
     </div>
-    <img
-      src="https://api.thegreenwebfoundation.org/greencheckimage/akupiec.work?nocache=true"
-      alt="This website is hosted Green - checked by thegreenwebfoundation.org"
-    />
+
     <div class="rights">© 2023 akupiec. All Rights Reserved.</div>
   </footer>
 );
--- src/shared/footer/footer.tsx
+++ src/shared/footer/footer.tsx
@@ -22,13 +22,22 @@ export const Footer = ({ topId }: { topId?: string }) => {
           <a
             class="social linkedin"
             aria-label="Open linkedin profile"
-            href="https://www.linkedin.com/in/alicja-kupiec/">
+            href="https://www.linkedin.com/in/alicja-kupiec/"
+            target="_blank">
             <img src="/assets/social/linkedin.svg" alt="linkedin" role="presentation" />
           </a>
-          <a class="social figma" aria-label="Open figma profile" href="https://www.figma.com/@alicjakupiec">
+          <a
+            class="social figma"
+            aria-label="Open figma profile"
+            href="https://www.figma.com/@alicjakupiec"
+            target="_blank">
             <img src="/assets/social/figma.svg" alt="figma" role="presentation" />
           </a>
-          <a class="social behance" aria-label="Open behance profile" href="https://www.behance.net/alicjakupiec">
+          <a
+            class="social behance"
+            aria-label="Open behance profile"
+            href="https://www.behance.net/alicjakupiec"
+            target="_blank">
             <img src="/assets/social/behance.svg" alt="behance" role="presentation" />
           </a>
         </div>
